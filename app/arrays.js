923ab4143117e3ee5c7ff9d8e80b5e3efa594cf4
Gabriel Craveiro <gabriel.c.craveiro@gmail.com>
2017-05-21 11:58:18 -0300

    Exercicios de array

--- app/arrays.js
+++ app/arrays.js
@@ -1,59 +1,62 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
-  indexOf: function(arr, item) {
+  indexOf: function (arr, item) {
+    return arr.indexOf(item);
+  },
+
+  sum: function (arr) {
+    return arr.reduce((a, b) => a + b);
+  },
+
+  remove: function (arr, item) {
+    return arr.filter(x => x !== item);
+  },
+
+  removeWithoutCopy: function (arr, item) {
+  },
+
+  append: function (arr, item) {
+    let arr2 = arr;
+    arr2.push(item);
+    return arr2;
+  },
+
+  truncate: function (arr) {
+    let arr2 = arr;
+    arr2.pop();
+    return arr2;
+  },
+
+  prepend: function (arr, item) {
 
   },
 
-  sum: function(arr) {
+  curtail: function (arr) {
 
   },
 
-  remove: function(arr, item) {
+  concat: function (arr1, arr2) {
 
   },
 
-  removeWithoutCopy: function(arr, item) {
+  insert: function (arr, item, index) {
 
   },
 
-  append: function(arr, item) {
+  count: function (arr, item) {
 
   },
 
-  truncate: function(arr) {
+  duplicates: function (arr) {
 
   },
 
-  prepend: function(arr, item) {
+  square: function (arr) {
 
   },
 
-  curtail: function(arr) {
-
-  },
-
-  concat: function(arr1, arr2) {
-
-  },
-
-  insert: function(arr, item, index) {
-
-  },
-
-  count: function(arr, item) {
-
-  },
-
-  duplicates: function(arr) {
-
-  },
-
-  square: function(arr) {
-
-  },
-
-  findAllOccurrences: function(arr, target) {
+  findAllOccurrences: function (arr, target) {
 
   }
 };
