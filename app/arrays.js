a78e9dfaa7719334cb6db4c6f51a001f0b23df7c
Gabriel Craveiro <gabriel.c.craveiro@gmail.com>
2017-05-26 19:19:49 -0300

    fiz mais algumas funções de array como pratica diaria

--- app/arrays.js
+++ app/arrays.js
@@ -14,46 +14,59 @@ exports.arraysAnswers = {
   },
 
   removeWithoutCopy: function (arr, item) {
+    return arr;
   },
 
   append: function (arr, item) {
-    let arr2 = arr;
-    arr2.push(item);
-    return arr2;
+    arr.push(item);
+    return arr;
   },
 
   truncate: function (arr) {
-    let arr2 = arr;
-    arr2.pop();
-    return arr2;
+    arr.pop();
+    return arr;
   },
 
   prepend: function (arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail: function (arr) {
-
+    let arr2 = arr;
+    arr2.shift();
+    return arr2;
   },
 
   concat: function (arr1, arr2) {
-
+    let arrConcat = arr1.concat(arr2);
+    return arrConcat;
   },
 
   insert: function (arr, item, index) {
-
+    arr.splice(index, 0, item);
+    return arr;
   },
 
   count: function (arr, item) {
-
+    let counter = 0;
+    arr.map(x => {
+      if (x === item) {
+        counter++;
+      }
+    });
+    return counter;
   },
 
   duplicates: function (arr) {
-
   },
 
   square: function (arr) {
-
+    arr.forEach(x =>{
+      x *= x;
+    });
+    console.log(arr);
+    return arr;
   },
 
   findAllOccurrences: function (arr, target) {
